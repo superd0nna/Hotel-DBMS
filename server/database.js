a3af55f0eeda54f3738dec6c40f3e97b79cc9e1f
Matthew Bitar <mattrabbi7@gmail.com>
2024-03-29 19:11:39 -0400

    added correct database connection

--- server/database.js
+++ server/database.js
@@ -5,17 +5,42 @@ const pool = new Pool({
     user: "postgres",
     password: "alskdjfhg",
     host: "localhost",
+    database: 'postgres',
     port: 5432,
 });
 
-pool
-.query("CREATE DATABASE ehotel2;")
-.then((Response) => {
-    console.log("Database created")
-    console.log(response)
-})
-.catch((err) => {
-    console.log("Error creating the database: ", err);
+
+pool.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
 });
+
+
+pool.query('Select * from ehotel.hotel_chain', (err, res)=>{
+    if (!err){
+        console.log(res.rows);
+    } else{
+        console.log(err.message);
+    }
+    pool.end;
+})
+
+// pool
+// .query("SELECT * FROM ehotel.hotel_chain")
+// .then((Response) => {
+//     console.log("Query successful")
+//     console.log(response.rows)
+// })
+// .catch((err) => {
+//     console.log("Error running query: ", err);
+// });
+// .query("CREATE DATABASE ehotel2;")
+// .then((Response) => {
+//     console.log("Database created")
+//     console.log(response)
+// })
+// .catch((err) => {
+//     console.log("Error creating the database: ", err);
+// });
 
 module.exports = pool;
